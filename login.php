f40aecb9584aa8245d312c02d5496a78d8e8943c
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-09-03 00:12:13 +0530

    Create login.php
    user(admin) login

---
+++ login.php
@@ -0,0 +1,15 @@
+<?php
+include "./header.php";
+include "./navbar1.php";
+include "./footer.php";
+?>
+<form action="./script.php" method="post">
+    <label for="LoginUsername">Username:</label>
+    <input type="text" name="username" id="LoginUsername">
+
+    <label for="LoginPassword">Password:</label>
+    <input type="password" name="password" id="LoginPassword">
+
+    <input type="submit" value="Login" name="loginButton">
+
+</form>
