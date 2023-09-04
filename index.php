f0f0317cde6c8b629ea611eafa12ed430f13e0b0
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-09-05 00:16:47 +0530

    Create index.php
    This is a index.php. It includes welcome description of the website.

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include "./header.php";
+include "./navbar1.php";
+include "./footer.php";
+?>
+
+<p>Welcome!</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore esse nobis, sed optio nulla enim quasi accusamus exercitationem eligendi sapiente tempora sequi cum eaque a suscipit eos commodi mollitia similique.</p>
