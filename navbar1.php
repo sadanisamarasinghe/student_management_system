b5649a80b45ef5a90252422decb5846c1c8afec0
sadani <129391563+sadanisamarasinghe@users.noreply.github.com>
2023-09-11 23:01:51 +0530

    This is my navbar1.php
    Which is used to before login page he /she can display this page.

---
+++ navbar1.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <h2>Student Management System</h2>
+        <ul>
+            <li><a href="./login.php"><button>Log In</button></a></li>
+        </ul>
+    </nav>
+</header>
